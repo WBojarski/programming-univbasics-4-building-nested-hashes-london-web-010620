8b639f0278988cbb08c2d5e841046d32ad3921fa
Wojciech Bojarski <bojawojt@gmail.com>
2019-12-19 10:49:05 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,11 +19,11 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-    new_hash = {
+  new_hash = {
     railroads: {
       pieces: 4,
       names: {
-        reading_railroad: 0,
+        reading_railroad: {},
         rail2: 0,
         rail3: 0,
         rail4: 0
