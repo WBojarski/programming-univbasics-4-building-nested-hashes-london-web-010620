eff3c9104dd2f308f206a3e69f5929588239e9f2
Wojciech Bojarski <bojawojt@gmail.com>
2019-12-19 10:38:48 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,10 +23,10 @@ def monopoly_with_third_tier
     railroads: {
       pieces: 4,
       rent_in_dollars: {
-        key1:
-        key2:
-        key3:
-        key4:
+        key1: 0
+        key2: 0
+        key3: 0
+        key4: 0
 
       }
     }
